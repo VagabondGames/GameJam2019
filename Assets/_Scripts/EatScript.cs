2ceb909ed43eaf09983e6a79e498ce293ead84cf
Allan Park <acpark@vagabond-games.com>
2019-01-27 14:59:29 -0800

    JUSTICE COMMIT!!

---
+++ Assets/_Scripts/EatScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatScript : MonoBehaviour {
+
+    public AudioSource eatSound;
+
+
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("omg!");
+        
+        if (collision.gameObject.CompareTag("HotDogBite"))
+        { 
+            eatSound.Play();
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Scripts/GameEngine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class GameEngine : MonoBehaviour {
+
+    public bool isTrackingDisabled = true;
+    public bool isMovementNegated = false;
+    public bool isRotationNegated = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        UnityEngine.XR.InputTracking.disablePositionalTracking = isTrackingDisabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovementNegated)
+        {
+            Vector3 verticalCameraMovement = new Vector3 (-InputTracking.GetLocalPosition(XRNode.CenterEye).x, 0, -InputTracking.GetLocalPosition(XRNode.CenterEye).z);
+
+            transform.position = verticalCameraMovement;
+
+            //transform.position = -InputTracking.GetLocalPosition(XRNode.CenterEye);
+        }
+
+        if (isRotationNegated)
+            transform.rotation = Quaternion.Inverse(InputTracking.GetLocalRotation(XRNode.CenterEye));
+    }
+}
